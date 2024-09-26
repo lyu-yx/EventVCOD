6260c79b8e280a9a4012a244c9a7404f77f4fd37
lyu-yx <lvyixuan@njust.edu.cn>
2024-09-26 10:11:57 +0800

    refactor: Comment out PointDecoder class and its methods in sam.py

--- models/sam.py
+++ models/sam.py
@@ -414,90 +414,90 @@ class SAM(nn.Module):
                     param.requires_grad = requires_grad
 
 
-class PointDecoder(nn.Module):
-    def __init__(self, sam) -> None:
-        super().__init__()
-        transformer_dim = 256
-        activation = nn.GELU
-        self.transformer = TwoWayTransformer(
-            depth=2,
-            embedding_dim=transformer_dim,
-            mlp_dim=2048,
-            num_heads=8,
-        )
-        self.mask_tokens = nn.Embedding(1, transformer_dim)
-        self.output_upscaling = nn.Sequential(
-            nn.ConvTranspose2d(transformer_dim, transformer_dim // 4, kernel_size=2, stride=2),
-            LayerNorm2d(transformer_dim // 4),
-            activation(),
-            nn.ConvTranspose2d(transformer_dim // 4, transformer_dim // 8, kernel_size=2, stride=2),
-            activation(),
-        )
-        self.output_hypernetworks_mlp = MLP(transformer_dim, transformer_dim, transformer_dim // 8, 3)
-        self.transformer.load_state_dict(sam.mask_decoder.transformer.state_dict())
-        self.output_upscaling.load_state_dict(sam.mask_decoder.output_upscaling.state_dict())
-        self.output_hypernetworks_mlp.load_state_dict(sam.mask_decoder.output_hypernetworks_mlps[0].state_dict())
-        # from ops.foundation_models.segment_anything.modeling.prompt_encoder import PositionEmbeddingRandom
-        embed_dim = 256
-        self.image_embedding_size = (64, 64)
-        self.pe_layer = PositionEmbeddingRandom(embed_dim // 2)
-        self.nms_kernel_size = 3
-        self.point_threshold = 0.1
-        self.max_points = 1000
+# class PointDecoder(nn.Module):
+#     def __init__(self, sam) -> None:
+#         super().__init__()
+#         transformer_dim = 256
+#         activation = nn.GELU
+#         self.transformer = TwoWayTransformer(
+#             depth=2,
+#             embedding_dim=transformer_dim,
+#             mlp_dim=2048,
+#             num_heads=8,
+#         )
+#         self.mask_tokens = nn.Embedding(1, transformer_dim)
+#         self.output_upscaling = nn.Sequential(
+#             nn.ConvTranspose2d(transformer_dim, transformer_dim // 4, kernel_size=2, stride=2),
+#             LayerNorm2d(transformer_dim // 4),
+#             activation(),
+#             nn.ConvTranspose2d(transformer_dim // 4, transformer_dim // 8, kernel_size=2, stride=2),
+#             activation(),
+#         )
+#         self.output_hypernetworks_mlp = MLP(transformer_dim, transformer_dim, transformer_dim // 8, 3)
+#         self.transformer.load_state_dict(sam.mask_decoder.transformer.state_dict())
+#         self.output_upscaling.load_state_dict(sam.mask_decoder.output_upscaling.state_dict())
+#         self.output_hypernetworks_mlp.load_state_dict(sam.mask_decoder.output_hypernetworks_mlps[0].state_dict())
+#         # from ops.foundation_models.segment_anything.modeling.prompt_encoder import PositionEmbeddingRandom
+#         embed_dim = 256
+#         self.image_embedding_size = (64, 64)
+#         self.pe_layer = PositionEmbeddingRandom(embed_dim // 2)
+#         self.nms_kernel_size = 3
+#         self.point_threshold = 0.1
+#         self.max_points = 1000
 
-    def get_dense_pe(self) -> torch.Tensor:
-        return self.pe_layer(self.image_embedding_size).unsqueeze(0)
+#     def get_dense_pe(self) -> torch.Tensor:
+#         return self.pe_layer(self.image_embedding_size).unsqueeze(0)
 
-    def forward(self, image_embeddings, masks=None):
-        output_tokens = self.mask_tokens.weight[0].unsqueeze(0)
-        sparse_embeddings = output_tokens.unsqueeze(0).expand(image_embeddings.size(0), -1, -1)
-        image_pe = self.get_dense_pe()
-        src = image_embeddings
-        pos_src = image_pe
-        b, c, h, w = src.shape
-        hs, src = self.transformer(src, pos_src, sparse_embeddings)
-        src = src.transpose(1, 2).view(b, c, h, w)
-        mask_tokens_out = hs[:, 0, :]
-        upscaled_embedding = self.output_upscaling(src)
-        hyper_in = self.output_hypernetworks_mlp(mask_tokens_out).unsqueeze(1)
-        b, c, h, w = upscaled_embedding.shape
-        pred_heatmaps = (hyper_in @ upscaled_embedding.view(b, c, h * w)).view(b, -1, h, w)
-        if self.training:
-            return {'pred_heatmaps': pred_heatmaps}
+#     def forward(self, image_embeddings, masks=None):
+#         output_tokens = self.mask_tokens.weight[0].unsqueeze(0)
+#         sparse_embeddings = output_tokens.unsqueeze(0).expand(image_embeddings.size(0), -1, -1)
+#         image_pe = self.get_dense_pe()
+#         src = image_embeddings
+#         pos_src = image_pe
+#         b, c, h, w = src.shape
+#         hs, src = self.transformer(src, pos_src, sparse_embeddings)
+#         src = src.transpose(1, 2).view(b, c, h, w)
+#         mask_tokens_out = hs[:, 0, :]
+#         upscaled_embedding = self.output_upscaling(src)
+#         hyper_in = self.output_hypernetworks_mlp(mask_tokens_out).unsqueeze(1)
+#         b, c, h, w = upscaled_embedding.shape
+#         pred_heatmaps = (hyper_in @ upscaled_embedding.view(b, c, h * w)).view(b, -1, h, w)
+#         if self.training:
+#             return {'pred_heatmaps': pred_heatmaps}
 
-        if masks is not None:
-            pred_heatmaps *= masks
+#         if masks is not None:
+#             pred_heatmaps *= masks
 
-        with torch.no_grad():
-            from ops.ops import _nms
-            # pred_heatmaps_nms = _nms(pred_heatmaps.detach().sigmoid().clone(), self.nms_kernel_size)
-            pred_heatmaps_nms = _nms(pred_heatmaps.detach().clone(), self.nms_kernel_size)
-            pred_points, pred_points_score = torch.zeros(b, self.max_points, 2).cuda(), torch.zeros(b,
-                                                                                                    self.max_points).cuda()
-            m = 0
-            for i in range(b):
-                points = torch.nonzero((pred_heatmaps_nms[i] > self.point_threshold).squeeze())
-                points = torch.flip(points, dims=(-1,))
-                pred_points_score_ = pred_heatmaps_nms[i, 0, points[:, 1], points[:, 0]].flatten(0)
+#         with torch.no_grad():
+#             from ops.ops import _nms
+#             # pred_heatmaps_nms = _nms(pred_heatmaps.detach().sigmoid().clone(), self.nms_kernel_size)
+#             pred_heatmaps_nms = _nms(pred_heatmaps.detach().clone(), self.nms_kernel_size)
+#             pred_points, pred_points_score = torch.zeros(b, self.max_points, 2).cuda(), torch.zeros(b,
+#                                                                                                     self.max_points).cuda()
+#             m = 0
+#             for i in range(b):
+#                 points = torch.nonzero((pred_heatmaps_nms[i] > self.point_threshold).squeeze())
+#                 points = torch.flip(points, dims=(-1,))
+#                 pred_points_score_ = pred_heatmaps_nms[i, 0, points[:, 1], points[:, 0]].flatten(0)
 
-                idx = torch.argsort(pred_points_score_, dim=0, descending=True)[
-                      :min(self.max_points, pred_points_score_.size(0))]
-                # print(points.size(), pred_points_score_.size(),  idx, idx.max())
-                points = points[idx]
-                pred_points_score_ = pred_points_score_[idx]
-                # print(points.size(), pred_points_score_.size(), pred_points_score_)
-                # print(pred_points.size(), pred_points_score.size())
-                # print(i)
-                #
-                pred_points[i, :points.size(0)] = points
-                pred_points_score[i, :points.size(0)] = pred_points_score_
-                m = max(m, points.size(0))
-            # pred_points = (pred_points + 0.5) * 4
-            pred_points = pred_points[:, :m]
-            pred_points_score = pred_points_score[:, :m]
-            pred_points = pred_points * 4
+#                 idx = torch.argsort(pred_points_score_, dim=0, descending=True)[
+#                       :min(self.max_points, pred_points_score_.size(0))]
+#                 # print(points.size(), pred_points_score_.size(),  idx, idx.max())
+#                 points = points[idx]
+#                 pred_points_score_ = pred_points_score_[idx]
+#                 # print(points.size(), pred_points_score_.size(), pred_points_score_)
+#                 # print(pred_points.size(), pred_points_score.size())
+#                 # print(i)
+#                 #
+#                 pred_points[i, :points.size(0)] = points
+#                 pred_points_score[i, :points.size(0)] = pred_points_score_
+#                 m = max(m, points.size(0))
+#             # pred_points = (pred_points + 0.5) * 4
+#             pred_points = pred_points[:, :m]
+#             pred_points_score = pred_points_score[:, :m]
+#             pred_points = pred_points * 4
 
-        return {'pred_heatmaps': pred_heatmaps,
-                'pred_points': pred_points,
-                'pred_points_score': pred_points_score,
-                'pred_heatmaps_nms': pred_heatmaps_nms}
+#         return {'pred_heatmaps': pred_heatmaps,
+#                 'pred_points': pred_points,
+#                 'pred_points_score': pred_points_score,
+#                 'pred_heatmaps_nms': pred_heatmaps_nms}
