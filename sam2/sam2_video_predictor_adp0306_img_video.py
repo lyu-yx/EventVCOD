3a06b783e5f26f79f40e9f7cae97aa4fdc137473
lyu-yx <lvyixuan@njust.edu.cn>
2025-03-10 11:15:27 +0800

    refactor: remove unused variables and debug print statements in SAM2VideoPredictor

--- sam2/sam2_video_predictor_adp0306_img_video.py
+++ sam2/sam2_video_predictor_adp0306_img_video.py
@@ -376,9 +376,6 @@ class SAM2VideoPredictor(SAM2Base):
         is_cond = is_init_cond_frame or self.add_all_frames_to_correct_as_cond
         storage_key = "cond_frame_outputs" if is_cond else "non_cond_frame_outputs"
         
-
-        prev_sam_mask_logits = None
-
         prev_out = obj_temp_output_dict[storage_key].get(frame_idx)
         if prev_out is None:
             prev_out = obj_output_dict["cond_frame_outputs"].get(frame_idx)
@@ -392,10 +389,6 @@ class SAM2VideoPredictor(SAM2Base):
             prev_sam_mask_logits = torch.clamp(prev_sam_mask_logits, -32.0, 32.0)
 
 
-        print('in add_new_mask:')
-        print('is_init_cond_frame (should be true)', is_init_cond_frame)
-        print('frame idx', frame_idx)
-        
         if is_init_cond_frame:
             prev_sam_mask_logits = None
 
