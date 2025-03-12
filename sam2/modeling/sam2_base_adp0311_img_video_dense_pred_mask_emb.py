fix: update embedding loss calculation and remove unnecessary print statements

sam2/modeling/sam2_base_adp0311_img_video_dense_pred_mask_emb.py
@@ -466,16 +466,14 @@ class SAM2Base(torch.nn.Module):
             # this loss should work for all frame
 
             mask_resized = F.interpolate(mask, size=mask_inputs_pred.shape[-2:], mode='bilinear', align_corners=False)
-            print('mask_inputs not none')
-            embedding_loss = structure_loss(mask_inputs_pred, mask_resized)
+            embedding_loss = structure_loss(mask_inputs_pred, mask_resized, loss_on_multimask=False)
             
             
         else:
             dense_embeddings_input = dense_embeddings_gt
             sparse_embeddings_input = sparse_embeddings_gt
             
-            print('none')
-            embedding_loss = structure_loss(mask_inputs_pred, mask_inputs_pred)
+            embedding_loss = torch.zeros([], device=mask_inputs_pred.device, requires_grad=True)
 
         print('embedding_loss', embedding_loss)
         