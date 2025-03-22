88e57280ff444ff140bd1cdbdecb13e40b897715
Yixuan <59128252+lyu-yx@users.noreply.github.com>
2025-03-22 14:34:13 +0800

    fix: correct event feature inputs in SAM2Base forward method

--- sam2/modeling/sam2_base_adp0321_ablation_base.py
+++ sam2/modeling/sam2_base_adp0321_ablation_base.py
@@ -973,11 +973,11 @@ class SAM2Base(torch.nn.Module):
            
             sam_outputs, embedding_loss = self._forward_sam_heads(
                 backbone_features=pix_feat,
-                event_features=pix_feat_short_long,
+                event_features=pix_feat,
                 point_inputs=point_inputs,
                 mask_inputs=mask_inputs,
                 high_res_features=high_res_features_adp,
-                high_res_event_features=high_res_event_features_adp,
+                high_res_event_features=high_res_features_adp,
                 multimask_output=multimask_output,
                 is_init_cond_frame=is_init_cond_frame,
             )
